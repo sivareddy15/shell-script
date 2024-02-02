cbedd0f59dfbc8662903b2df25580b313053187d
SIVAREDDY <sivareddy9059@gmail.com>
2024-02-02 23:42:25 +0530

    hiding password

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,7 +3,14 @@
  #DATE=$(date)
  #echo "Today Date is ${DATE}"
 
- PERSON1=$1
- PERSON2=$2
+#  PERSON1=$1
+#  PERSON2=$2
 
- echo "$PERSON1: HELLO $PERSON2, Good Morning!"
+#  echo "$PERSON1: HELLO $PERSON2, Good Morning!"
+
+echo "Enter user name"
+read USERNAME
+echo "Enter passowrd"
+read -s PASSWORD
+
+echo "Username : $USERNAME Password: $PASSWORD"
