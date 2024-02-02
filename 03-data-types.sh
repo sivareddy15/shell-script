f60a87cee6e81f3383039b304471c52d2a693f9a
SIVAREDDY <sivareddy9059@gmail.com>
2024-02-03 00:07:44 +0530

    datatypes changes added

--- 03-data-types.sh
+++ 03-data-types.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
- NUMBER1=$1
- NUMBER2=$2
+#  NUMBER1=$1
+#  NUMBER2=$2
 
- SUM=$(($NUMBER1+$NUMBER2))
+#  SUM=$(($NUMBER1+$NUMBER2))
 
- echo "Total sum is :: $SUM"
+#  echo "Total sum is :: $SUM"
+
+ FRUITS=("APPLE","BANANA","Mango")
+
+ echo "First fruit is ${FRUITS[0]}"
+ echo "SECOND FRIUIT IS ${FRUITS[1]}"
+ echo "THIRD FRIUT IS $FRUITS[2]"
