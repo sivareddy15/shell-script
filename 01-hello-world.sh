05795da5af6a883a8ca6f4fbd42132eda0213460
SIVAREDDY <sivareddy9059@gmail.com>
2024-02-02 16:13:50 +0530

    variables scripts commit

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+PERSON1="RAMU"
+PERSON2="RAJU"
 
-echo "hello shell script"
+echo "$PERSON1 : hello $PERSON2, Good Morning"
+echo "$PERSON2 : Hi $PERSON1, Very Good Morning"
+echo "$PERSON1 : $PERSON2, How are you?"
+echo "$PERSON2  : I am good $PERSON1, How are you?"
